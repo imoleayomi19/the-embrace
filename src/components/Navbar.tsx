aa9d62ef3a996e61f8e31b142fd86058405aa563
Emmanuel Imole <imolee98@gmail.com>
2026-07-30 16:54:30 +0100

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -392,7 +392,7 @@ export function Navbar() {
           {/* Logo - Far Left */}
           <Link to="/" className="group flex-shrink-0 min-w-0">
             <img
-              src="./yellow1-removebg-preview.png"
+              src="./orange1-removebg-preview.png"
               alt="Embrace Technologies"
               className={`
                 rounded-[20px] object-contain transition-all duration-300 group-hover:scale-105 max-w-full
